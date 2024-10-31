8ab497d57374465d88dada1cddcac987c1525f6f
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-10-31 17:19:25 +0530

    manage usr access to role user

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,7 +1,7 @@
 import { Role } from '@prisma/client';
 
 const allRoles = {
-  [Role.USER]: ['manageEvents', 'manageThreads', 'manageBlobUploads'],
+  [Role.USER]: ['manageEvents', 'manageThreads', 'manageBlobUploads', 'manageUsers'],
   [Role.ADMIN]: ['getUsers', 'manageUsers']
 };
 
